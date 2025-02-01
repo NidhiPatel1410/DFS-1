ad12a21224dceae89bae33a9c0ef6c62c2eb0006
Nidhi Patel <patelnidhim14@gmail.com>
2025-02-01 17:05:25 -0500

    completed

--- FloodFill.java
+++ FloodFill.java
@@ -38,3 +38,32 @@ class Solution {
         return image;
     }
 }
+
+// DFS:
+class Solution {
+    int m, n;
+    int[][] dirs;
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image == null || image.length == 0 || image[sr][sc] == color) {
+            return image;
+        }
+        m = image.length;
+        n = image[0].length;
+        dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        dfs(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+
+    private void dfs(int[][] image, int sr, int sc, int color, int oldcolor) {
+        if (sr < 0 || sr == m || sc < 0 || sc == n || image[sr][sc] != oldcolor) {
+            return;
+        }
+        image[sr][sc] = color;
+        for (int[] dir : dirs) {
+            int nr = sr + dir[0];
+            int nc = sc + dir[1];
+            dfs(image, nr, nc, color, oldcolor);
+        }
+    }
+}
